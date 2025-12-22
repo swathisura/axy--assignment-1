8cfa0b4256c438508acf6d787adf24c402fcf1c1
swathisura <swathisura65@gmail.com>
2025-12-22 16:42:28 +0530

    Create ec2.tf

---
+++ terraform/ec2.tf
@@ -0,0 +1,14 @@
+resource "aws_launch_template" "backend" {
+  name_prefix   = "axy-backend"
+  image_id      = "ami-0c02fb55956c7d316"
+  instance_type = "t3.micro"
+
+  user_data = base64encode(<<EOF
+#!/bin/bash
+yum install -y docker
+systemctl start docker
+docker run -d -p 5000:5000 your-backend-image
+EOF)
+
+  vpc_security_group_ids = [aws_security_group.backend_sg.id]
+}
