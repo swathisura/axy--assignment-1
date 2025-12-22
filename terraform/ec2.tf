213758bb1c4a5daab8e58899059a12fb51c7f51b
swathisura <swathisura65@gmail.com>
2025-12-22 17:44:29 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,16 +3,15 @@ resource "aws_launch_template" "backend" {
   image_id      = "ami-0c02fb55956c7d316"
   instance_type = "t3.micro"
 
-  user_data = base64encode(<<EOF
-#!/bin/bash
-yum install -y docker
-systemctl enable docker
-systemctl start docker
-sleep 10
-docker run -d -p 5000:5000 your-backend-image
+ user_data = base64encode(<<-EOF
+  #!/bin/bash
+  yum install -y docker
+  systemctl enable docker
+  systemctl start docker
+  sleep 10
+  docker run -d -p 5000:5000 your-backend-image
 EOF
-  )
-
-  vpc_security_group_ids = [aws_security_group.backend_sg.id]
+)
+ vpc_security_group_ids = [aws_security_group.backend_sg.id]
 }
 
