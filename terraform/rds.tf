f9b794ec1965940087bce2ccdf48203c74e6bbdb
swathisura <swathisura65@gmail.com>
2025-12-22 18:35:13 +0530

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,8 +1,8 @@
 resource "aws_db_instance" "postgres" {
   engine            = "postgres"
-  instance_class    = "db.t3.micro"
+  instance_class    = "db.t3.small"
   allocated_storage = 20
-  username          = "admin"
+  username          = "myadmin"
   password          = "password123"
   multi_az          = true
   skip_final_snapshot = true
